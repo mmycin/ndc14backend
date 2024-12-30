c273c5be2d4f718f3cea35b5982cc510dad77e42
Tahcin Ul Karim Mycin <mycin.mit@gmail.com>
2024-12-30 21:01:31 +0600

    final

--- config/loadEnv.go
+++ config/loadEnv.go
@@ -8,10 +8,18 @@ import (
 )
 
 func LoadEnv() {
+	// Skip loading .env file if we're in production
+	if os.Getenv("GO_ENV") == "production" {
+		libs.Success("Running in production mode - using environment variables")
+		return
+	}
+
+	// Load .env file for development
 	err := godotenv.Load()
 	if err != nil {
 		cwd, _ := os.Getwd()
-		libs.Fatal("Error loading .env file from " + cwd)
+		libs.Warning("No .env file found in " + cwd + " - using environment variables")
+		return // Don't fatal in this case, just warn
 	}
 	libs.Success("Loaded .env file")
 }
